e72d5e134cf29212917ebb27c393c40af7a86aa5
indmind <sattanationmail@gmail.com>
2020-03-21 13:54:16 +0700

    change default box-sizing to border-box

--- src/utils/componentTemplate.js
+++ src/utils/componentTemplate.js
@@ -1,7 +1,22 @@
 export default function (html, css) {
     const template = document.createElement('template');
 
-    template.innerHTML = `<style>${css}</style>${html}`
+    template.innerHTML = `
+        <style>
+            *::after,
+            *::before {
+                box-sizing: inherit;
+            }
+
+            * {
+                box-sizing: border-box;
+            }
+
+            ${css}
+        </style>
+
+        ${html}
+    `
 
     return template
 }
